b4ca850c7dc2fb03a24e4a8c3c7a3628d7279c0c
_phalt_ <30666498+lynnin@users.noreply.github.com>
2026-02-03 17:00:49 +0800

    Create EmojiTrackerApp.swift

---
+++ EmojiTrackerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct EmojiTrackerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
